8eab2df945ba874055ed97f0cf2a5ae00e6cac66
luoyexunxue <ye_luo@qq.com>
2021-03-22 15:52:41 +0800

    add async task class

---
+++ Avatar/CAsyncTask.cpp
@@ -0,0 +1,104 @@
+﻿//================================================
+// Copyright (c) 2021 周仁锋. All rights reserved.
+// ye_luo@qq.com
+//================================================
+#include "CAsyncTask.h"
+#include "CEngine.h"
+
+/**
+* 默认构造函数
+*/
+CAsyncTask::CAsyncTask(CEngine* engine) {
+	m_bCancel = false;
+	m_pEngine = engine;
+	m_pTaskHandler = new CTaskHandler(this);
+	m_pThread = new CThread();
+	m_pEngine->RegisterEvent("update", m_pTaskHandler);
+}
+
+/**
+* 默认析构函数
+*/
+CAsyncTask::~CAsyncTask() {
+	m_pEngine->UnRegisterEvent("update", m_pTaskHandler, true);
+	delete m_pThread;
+}
+
+/**
+* 任务进度通知
+* @param percent 进度百分比
+* @note 被主线程调用
+*/
+void CAsyncTask::OnProgress(int percent) {
+}
+
+/**
+* 任务完成通知
+* @param cancel 是否被用户取消
+* @note 被主线程调用
+*/
+void CAsyncTask::OnCompleted(bool cancel) {
+}
+
+/**
+* 启动异步任务
+* @note 请在主线程中调用
+*/
+void CAsyncTask::Run() {
+	m_pThread->Start(CAsyncTask::RunBackground, this);
+}
+
+/**
+* 取消异步任务
+*/
+void CAsyncTask::Cancel() {
+	m_pThread->Lock();
+	m_bCancel = true;
+	m_pThread->Unlock();
+}
+
+/**
+* 报告进度
+* @note 请在异步线程中调用
+*/
+void CAsyncTask::ReportProgress(int percent) {
+	m_pThread->Lock();
+	m_queEvents.push(STaskEvent(percent));
+	m_pThread->Unlock();
+}
+
+/**
+* 是否已取消
+*/
+bool CAsyncTask::IsCancel() {
+	return m_bCancel;
+}
+
+/**
+* 后台线程方法
+*/
+void CAsyncTask::RunBackground(class CThread* thread, void* param) {
+	CAsyncTask* task = reinterpret_cast<CAsyncTask*>(param);
+	task->DoWork();
+	task->m_pThread->Lock();
+	task->m_queEvents.push(STaskEvent(task->m_bCancel));
+	task->m_pThread->Unlock();
+}
+
+/**
+* 任务事件处理
+*/
+void CAsyncTask::CTaskHandler::Handle() {
+	while (true) {
+		STaskEvent event;
+		m_pAsyncTask->m_pThread->Lock();
+		if (!m_pAsyncTask->m_queEvents.empty()) {
+			event = m_pAsyncTask->m_queEvents.front();
+			m_pAsyncTask->m_queEvents.pop();
+		}
+		m_pAsyncTask->m_pThread->Unlock();
+		if (event.eventType == 1) m_pAsyncTask->OnProgress(event.percentage);
+		else if (event.eventType == 2) m_pAsyncTask->OnCompleted(event.cancellation);
+		else break;
+	}
+}
---
+++ Avatar/CAsyncTask.h
@@ -0,0 +1,74 @@
+﻿//================================================
+// Copyright (c) 2021 周仁锋. All rights reserved.
+// ye_luo@qq.com
+//================================================
+#ifndef _CASYNCTASK_H_
+#define _CASYNCTASK_H_
+#include "AvatarConfig.h"
+#include "CEngine.h"
+#include "CThread.h"
+
+/**
+* @brief 异步任务工具类
+*/
+class AVATAR_EXPORT CAsyncTask {
+public:
+	//! 默认构造函数
+	CAsyncTask(CEngine* engine);
+	//! 默认析构函数
+	virtual ~CAsyncTask();
+
+	//! 任务进度通知
+	virtual void OnProgress(int percent);
+	//! 任务完成通知
+	virtual void OnCompleted(bool cancel);
+	//! 启动异步任务
+	virtual void Run();
+	//! 取消异步任务
+	virtual void Cancel();
+
+	//! 报告进度
+	virtual void ReportProgress(int percent);
+	//! 是否已取消
+	virtual bool IsCancel();
+	//! 异步处理实现
+	virtual void DoWork() = 0;
+
+private:
+	//! 后台线程方法
+	static void RunBackground(class CThread* thread, void* param);
+
+	//! 事件通知接口
+	class CTaskHandler : public CEngine::CNotifyHandler {
+	public:
+		//! 异步任务对象
+		CAsyncTask* m_pAsyncTask;
+		//! 默认构造函数
+		CTaskHandler(CAsyncTask* task): m_pAsyncTask(task) {}
+		//! 事件通知方法
+		virtual void Handle();
+	};
+	//! 事件定义
+	typedef struct _STaskEvent {
+		int eventType;
+		int percentage;
+		bool cancellation;
+		_STaskEvent(): eventType(-1) {}
+		_STaskEvent(int percent): eventType(1), percentage(percent) {}
+		_STaskEvent(bool cancel): eventType(2), cancellation(cancel) {}
+	} STaskEvent;
+
+private:
+	//! 引擎对象指针
+	CEngine* m_pEngine;
+	//! 任务事件回调
+	CTaskHandler* m_pTaskHandler;
+	//! 线程对象
+	CThread* m_pThread;
+	//! 是否已经被取消
+	bool m_bCancel;
+	//! 事件队列
+	queue<STaskEvent> m_queEvents;
+};
+
+#endif
